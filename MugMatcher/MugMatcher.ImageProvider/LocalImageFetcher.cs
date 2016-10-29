b97cc51d3f5ee2e940419cb0ddfdf210f55bb77e
Richard Hopwood <r.hopwood@fanatics.co.uk>
2016-10-29 15:49:02 +0100

    Local image finder

--- MugMatcher/MugMatcher.ImageProvider/LocalImageFetcher.cs
+++ MugMatcher/MugMatcher.ImageProvider/LocalImageFetcher.cs
@@ -1,9 +1,18 @@
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
 
 namespace MugMatcher.ImageProvider
 {
     public class LocalImageFetcher : IImageFetcher
     {
-        public IEnumerable<ImageFetchResult> Fetch(ImageFetchRequest request) { yield break; }
+        private const string ImageFileExtentions = "*.jpg;*.png;*.gif;*.tiff;*.jpeg";
+
+        public IEnumerable<ImageFetchResult> Fetch(ImageFetchRequest request)
+        {
+            var directoryInfo = new DirectoryInfo(ConfigurationManager.AppSettings["LocalImageSearchPath"]);
+            return directoryInfo.GetFiles(ImageFileExtentions).Select(info => new LocalImageFetchResult(info.FullName));
+        }
     }
 }

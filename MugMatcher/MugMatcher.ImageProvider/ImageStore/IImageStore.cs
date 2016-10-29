a4bbfbfff295d4111988084c670b1940a4724cde
matty3w <matty3w@yahoo.com>
2016-10-29 16:11:23 +0100

    image storage

---
+++ MugMatcher/MugMatcher.ImageProvider/ImageStore/IImageStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MugMatcher.ImageProvider
+{
+    public interface IImageStore
+    {
+        void AddRemoteImage(string url);
+
+        void AddLocalImage(string uri);
+
+        List<string> GetAllImagePaths();
+
+    }
+}

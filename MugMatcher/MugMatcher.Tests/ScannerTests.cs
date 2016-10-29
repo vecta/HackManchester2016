61c79251241c0519b464ce7c35c10fba4862441b
Robert Marshall <github@robware.uk>
2016-10-29 17:39:06 +0100

    Attempt at facial matching, currently can't load a DLL

---
+++ MugMatcher/MugMatcher.Tests/ScannerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace MugMatcher.Tests
+{
+	[TestFixture]
+    public class ScannerTests
+    {
+	    [Test]
+	    public void Scan_WithMultipleFaceImage_ReturnsFaces()
+	    {
+		    const string faceToMatch = @"TestImages\MissingPeople\Matt.jpg";
+		    const string imageToSearch = @"TestImages\ImagesToScan\IMG_20161029_151612.jpg";
+		    var scanner = new Scanner();
+		    var result = scanner.Scan(faceToMatch, imageToSearch);
+			Assert.That(result.Score, Is.GreaterThan(0));
+	    }
+    }
+}
---
+++ MugMatcher/MugMatcher/Scanner.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Neurotec.Biometrics;
+using Neurotec.Biometrics.Client;
+using Neurotec.Licensing;
+
+namespace MugMatcher
+{
+    public class Scanner
+    {
+	    const string BiometricsComponents= "Biometrics.FaceExtraction,Biometrics.FaceMatching";
+
+		private static NSubject CreateSubject(string fileName, bool isMultipleSubjects)
+		{
+			var subject = new NSubject { IsMultipleSubjects = isMultipleSubjects };
+			var face = new NFace { FileName = fileName };
+			subject.Faces.Add(face);
+			return subject;
+		}
+
+		public ScanResult Scan(string referencePath, string candidatePath)
+	    {
+		    NLicense.ObtainComponents("/local", 5000, BiometricsComponents);
+
+			var biometricClient = new NBiometricClient();
+		    var reference = CreateSubject(referencePath, false);
+		    var candidate = CreateSubject(candidatePath, true);
+		    var result=new ScanResult();
+
+		    var enrollTask = biometricClient.CreateTask(NBiometricOperations.Enroll, null);
+		    enrollTask.Subjects.Add(candidate);
+			biometricClient.PerformTask(enrollTask);
+
+		    result.Status = biometricClient.Identify(reference);
+		    result.Score = reference.MatchingResults[0].Score;
+
+		    return result;
+	    }
+    }
+}

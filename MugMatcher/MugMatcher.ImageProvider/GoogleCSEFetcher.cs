a41d81a59821ef1ab389d56e691a0ace3fbcaf64
matty3w <matty3w@yahoo.com>
2016-10-29 21:43:08 +0100

    google api

---
+++ MugMatcher/MugMatcher.ImageProvider/GoogleCSEFetcher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Tweetinvi.Logic.JsonConverters;
+
+namespace MugMatcher.ImageProvider
+{
+    public class GoogleCSEFetcher : IImageFetcher
+    {
+        private string CseUrl = "https://www.googleapis.com/customsearch/v1?";
+        private string APIKey;
+        private string CustomSearchEngineId;
+
+        public IEnumerable<ImageFetchResult> Fetch(ImageFetchRequest request)
+        {
+            var queryString = BuildQueryString(request);
+            APIKey = ConfigurationManager.AppSettings["GoogleAPIKey"];
+            CustomSearchEngineId = ConfigurationManager.AppSettings["CustomSearchEngineId"];
+            var data = GetFromUrl(queryString);
+            var results = JsonConvert.DeserializeObject<GoogleCSESearchResult>(data);
+            return results.items.Select(image => new ImageFetchResult(image.link, image.image.contextLink));
+        }
+
+        private string BuildQueryString(ImageFetchRequest request)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(CseUrl);
+            sb.Append($"key={APIKey}");
+            sb.Append($"&cx={CustomSearchEngineId}");
+            sb.Append($"q=crowd%20of%20people");
+            sb.Append($"&searchType=image");
+            sb.Append($"&imgType=face");
+            sb.Append($"&daterestrict={request.StartDate:ddmmyyyy}:{request.EndDate:ddmmyyyy}");
+
+            return sb.ToString();
+        }
+
+        private string GetFromUrl(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = request.GetResponse();
+            using (var responseStream = response.GetResponseStream())
+            {
+                var reader = new StreamReader(responseStream, Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}

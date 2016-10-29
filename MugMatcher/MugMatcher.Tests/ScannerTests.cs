c244eef78b0116631d6b0ff3accfb6384bef31e6
Robert Marshall <github@robware.uk>
2016-10-29 21:30:53 +0100

    Test image paths fixed

--- MugMatcher/MugMatcher.Tests/ScannerTests.cs
+++ MugMatcher/MugMatcher.Tests/ScannerTests.cs
@@ -10,8 +10,8 @@ namespace MugMatcher.Tests
 	    [Test]
 	    public void Scan_WithMultipleFaceImage_ReturnsFaces()
 	    {
-		    const string faceToMatch = @"..\TestImages\MissingPeople\Matt.jpg";
-		    const string imageToSearch = @"..\TestImages\ImagesToScan\IMG_20161029_152601.jpg";
+		    const string faceToMatch = @"TestImages\MissingPeople\Matt.jpg";
+		    const string imageToSearch = @"TestImages\ImagesToScan\IMG_20161029_152601.jpg";
 		    var scanner = new Scanner();
 		    var result = scanner.Scan(Path.GetFullPath(faceToMatch), Path.GetFullPath(imageToSearch));
 			Console.WriteLine(result.Score);
@@ -21,8 +21,8 @@ namespace MugMatcher.Tests
 	    [Test]
 	    public void Scan_WithNoMatchingFace_Returns0Score()
 		{
-			const string faceToMatch = @"..\TestImages\MissingPeople\Matt.jpg";
-			const string imageToSearch = @"..\TestImages\MissingPeople\IMG_20161029_140336.jpg";
+			const string faceToMatch = @"TestImages\MissingPeople\Matt.jpg";
+			const string imageToSearch = @"TestImages\MissingPeople\IMG_20161029_140336.jpg";
 			var scanner = new Scanner();
 			var result = scanner.Scan(Path.GetFullPath(faceToMatch), Path.GetFullPath(imageToSearch));
 			Console.WriteLine(result.Score);

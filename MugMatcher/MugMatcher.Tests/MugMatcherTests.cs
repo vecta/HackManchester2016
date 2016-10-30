faa4fd9e74a26efb5f8a2e02fe661731b735d50f
Richard Hopwood <r.hopwood@fanatics.co.uk>
2016-10-30 06:15:42 +0000

    no message

--- MugMatcher/MugMatcher.Tests/MugMatcherTests.cs
+++ MugMatcher/MugMatcher.Tests/MugMatcherTests.cs
@@ -13,10 +13,10 @@ namespace MugMatcher.Tests
 	    public void Find_WithLocalImageFetcher_FindsFace()
 	    {
             SetupKeys();
-            const string faceToMatch = @"C:\Users\Matthew\Documents\HackManchester2016\TestImages\MissingPeople\Matt.jpg";
-		    Acquisition acquisition =new Acquisition(new ImageStore());
+            const string faceToMatch = @"C:\Users\richard.hopwood\Documents\HackManchester2016\TestImages\MissingPeople\Matt.jpg";
+		    var acquisition =new Acquisition(new ImageStore());
 		    var matcher=new MugMatcher(acquisition);
-		    ImageFetchRequest imageFetchRequest=new ImageFetchRequest(null);
+		    var imageFetchRequest=new ImageFetchRequest(new GeoLocation());
 	        
             var found = matcher.Find(faceToMatch, imageFetchRequest);
 			Assert.That(found, Is.True);
@@ -24,18 +24,14 @@ namespace MugMatcher.Tests
 
         private void SetupKeys()
         {
-   
-           
-                ConfigurationManager.AppSettings["LocalImageSearchPath"] = @"ImageStore";
-            ConfigurationManager.AppSettings["TemporaryImageStorePath"] = @"C:\Users\robert.marshall.FIOFFICE\Documents\HackManchester\TestImages\ImagesToScan";
+            ConfigurationManager.AppSettings["TemporaryImageStorePath"] = @"F:\Temp\scanned"; //WHERE IMAGES DOWNLOADED OFF THE INTERNET ARE STORED
 
-            //ConfigurationManager.AppSettings["LocalImageSearchPath"] = @"C:\Temp\scanned";
+            ConfigurationManager.AppSettings["LocalImageSearchPath"] = @"C:\Users\richard.hopwood\Documents\HackManchester2016\TestImages\ImagesToScan"; //LOCAL IMAGES TO SCAN
             ConfigurationManager.AppSettings["TwitterAccessKey"] = "qLy8zHnWBYMC17x7IH6jx3fee";
             ConfigurationManager.AppSettings["TwitterSecretKey"] = "osxaNQhJbHvsfvEkKB4qsJnV4GbGAtFN65aQyJ3S1Sj3ClA6es";
             ConfigurationManager.AppSettings["InstagramAccessToken"] = "4092770693.e029fea.71fb21e304284dce9496cb4cdb635b8b";
             ConfigurationManager.AppSettings["GoogleAPIKey"] = "AIzaSyB-LOommQ-bfWysfqgnql6aOweiHy4KZCM";
             ConfigurationManager.AppSettings["CustomSearchEngineId"] = "002232642848038589219%3Aoizy299vm8k";
-            //ConfigurationManager.AppSettings["TemporaryImageStorePath"] = @"C:\Users\Matthew\Documents\HackManchester2016\TestImages";
             
         }
     }

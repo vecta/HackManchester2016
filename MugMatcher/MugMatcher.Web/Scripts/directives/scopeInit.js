e7b014a36a827839cf9819a48988594e0c968015
Robert Marshall <github@robware.uk>
2016-10-30 02:03:16 +0000

    Basic page to launch the person finder

---
+++ MugMatcher/MugMatcher.Web/Scripts/directives/scopeInit.js
@@ -0,0 +1,11 @@
+﻿mugMatcher.directive("scopeInit", function () {
+	return {
+		restrict: 'E',
+		link: function (scope, element, attributes) {
+			var content = element[0].innerHTML.trim();
+			scope[attributes.value] = JSON.parse(content);
+			element.remove();
+		}
+	}
+});
+
---
+++ MugMatcher/MugMatcher.Web/Scripts/find.js
@@ -0,0 +1,16 @@
+﻿mugMatcher.controller("find", ["$scope", "$http", function ($scope, $http) {
+	$scope.selectedPath = "";
+	$scope.personFound = null;
+
+	$scope.setPath = function (path) {
+		$scope.selectedPath = path;
+	}
+
+	$scope.findPerson = function (path) {
+		$scope.personFound = null;
+		$http.post("/find/findPerson", { file: path }).then(function (result) {
+			$scope.personFound = result.data;
+			console.log($scope.personFound);
+		});
+	}
+}]);
